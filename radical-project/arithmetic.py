4ab70c27940e166e7574f568f06fbed3183584ff
Lorcan Rae <lorcanrae@outlook.com>
2023-08-28 10:18:16 +0100

    added new fn add_any_number

--- radical-project/arithmetic.py
+++ radical-project/arithmetic.py
@@ -4,3 +4,9 @@ def add_two_numbers(a, b):
 
 def add_three_numbers(a, b, c) -> int:
     return a + b + c
+
+def add_any_numbers(*a):
+    sum_ = 0
+    for num in a:
+        sum_ += num
+    return sum_
